6cc0a955ed1a96b08662b61426fd9471141b6909
Aidan MacNevin <ahmacnevin@wpi.edu>
2023-09-11 19:09:06 -0400

    Added base attributes and functions for Box class

--- src/Box.java
+++ src/Box.java
@@ -1,3 +1,22 @@
 public class Box {
-    
+    private Line north;
+    private Line east;
+    private Line south;
+    private Line west;
+}
+
+    /**
+     * Check to see if the box is completed or not
+     * @return True if the box is completed
+     */
+    public boolean isComplete(){
+    return false;
+}
+
+    /**
+     * Check if the box is one line away from completion
+     * @return True if the box can be completed
+     */
+    public boolean isOneLineAway(){
+    return false;
 }
