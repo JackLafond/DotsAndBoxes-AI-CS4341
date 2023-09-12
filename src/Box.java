d1903bfd0d3a110b8d697d804be4ee6373b69b05
JackLafond <jacklafo12@gmail.com>
2023-09-12 10:55:10 -0400

    basic box

--- src/Box.java
+++ src/Box.java
@@ -3,20 +3,28 @@ public class Box {
     private Line east;
     private Line south;
     private Line west;
-}
+
+    public Box(Line north, Line east, Line south, Line west) {
+        this.north = north;
+        this.east = east;
+        this.south = south;
+        this.west = west;
+    }
 
     /**
      * Check to see if the box is completed or not
      * @return True if the box is completed
      */
     public boolean isComplete(){
-    return false;
-}
+        return north.isComplete && east.isComplete && south.isComplete && west.isComplete;
+    }
 
     /**
      * Check if the box is one line away from completion
      * @return True if the box can be completed
      */
     public boolean isOneLineAway(){
-    return false;
+        return (north.isComplete ? 1 : 0) + (east.isComplete ? 1 : 0) + (south.isComplete ? 1 : 0) + (west.isComplete ? 1 : 0) == 3;
+    }
+
 }
