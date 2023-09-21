4459b0ae0edc14b5a3cd84c70a75bb84dffd596d
JackLafond <jacklafo12@gmail.com>
2023-09-21 13:21:12 -0400

    fixing minimax and adding to baord

--- src/Board.java
+++ src/Board.java
@@ -1,4 +1,8 @@
 //Each Board will hold information of the current boardState
+
+import java.util.LinkedList;
+import java.util.List;
+
 public class Board {
 
     public static final int DOT = 8;
@@ -8,35 +12,42 @@ public class Board {
     public static final int BOARD_SIZE = 9;
 
 	int[][] boardState;
-	int maxlines;
+	int[] lastLine;
 	int playerscore;
 	int aiscore;
+	boolean myMove;
 
-	Board (int[][] state, int playerscore, int aiscore, boolean aimove) {
+	Board (int[][] state, int playerscore, int aiscore, boolean myMove) {
 		this.playerscore = playerscore;
 		this.aiscore = aiscore;
 		this.boardState = state;
+		this.myMove = myMove;
+		this.lastLine = null;
+	}
+
+	public void setLastLine(int[] aLine) {
+		this.lastLine = aLine;
 	}
 
 	public int[][] getState () {
-		return boardState;
+		return this.boardState;
 	}
 
 	public void updateplayerscore (int score) {
-		playerscore += score;
+		this.playerscore += score;
 	}
 
 	public void updateaiscore (int score) {
-		aiscore += score;
+		this.aiscore += score;
 	}
 
 	public int evaluate () {
-		return aiscore - this.playerscore;
+		return this.aiscore - this.playerscore;
 	}
-
+	
 	public void printboard() {
-		for (int i = 0; i < boardState.length; i++) {
-			for (int j = 0; j < boardState.length; j++) {
+		for (int i = 0; i < BOARD_SIZE; i++) {
+			for (int j = 0; j < BOARD_SIZE; j++) {
 				char cellValue;
 				switch (boardState[i][j]) {
 					case DOT:
@@ -64,49 +75,51 @@ public class Board {
         
 	}
 
-	public void updateState(int x, int y, int direction, int player){
-		this.boardState[x][y] = COMPLETED_LINE;
-		if(direction == 0){
-			//Horizontal Line, check boxes above and below
-			if(y != 0){
-				//Check box below
-				isBoxComplete(x, y-1, player);
+	public LinkedList<int[]> getLegalMoves() {
+
+		LinkedList<int[]> moves = new LinkedList<int[]>();
+
+		for(int row = 0; row < 19; row++) {
+			for(int col = 0; col < 19; col = col + 2) {
+                if(row % 2 == 0) {
+                    col = col + 1;
+                }
+				if(boardState[row][col] == Board.EMPTY_LINE) {
+					moves.add(new int[]{row, col});
+				}
 			}
-			if(y != 18){
-				//check box above
-				isBoxComplete(x, y+1, player);
-			}
-		} else{
-			//Vertical line, check boxes left and right
-			if(x != 0){
-				//Check box left
-				isBoxComplete(x-1, y, player);
-			}
-			if(x != 18){
-				//check box right
-				isBoxComplete(x+1, y, player);
-			}
+		}
+
+		return moves;
+	}
+
+	public boolean completeLine(int row, int col) {
+
+		boardState[row][col] = Board.COMPLETED_LINE;
+		setLastLine(new int[]{row, col});
+		if(row % 2 == 0) {
+			return checkBox(row-1, col) || checkBox(row+1, col);
+		} else {
+			return checkBox(row, col-1) || checkBox(row, col+1);
 		}
 	}
 
-	/**
-	 * Check to see if the box at this coordinate is complete or not. If so, update the value to the player id (1 or -1)
-	 * @param x x coordinate of box in state
-	 * @param y y coord of box in state
-	 * @param player player value (1 or -1)
-	 * @return true if the box is complete
-	 */
-	public boolean isBoxComplete(int x, int y, int player){
-		int topLine = this.boardState[x][y+1];
-		int bottomLine = this.boardState[x][y-1];
-		int rightLine = this.boardState[x+1][y];
-		int leftLine = this.boardState[x-1][y];
-
-		if(topLine == COMPLETED_LINE && bottomLine == COMPLETED_LINE && rightLine == COMPLETED_LINE && leftLine == COMPLETED_LINE){
-			this.boardState[x][y] = player;
-			return true;
+	public boolean checkBox(int row, int col) {
+		if(boardState[row][col] == Board.BLANK_SPACE) {
+			int sum = boardState[row][col + 1] + boardState[row][col - 1] + boardState[row - 1][col] + boardState[row + 1][col];
+			if(sum == 4 * Board.COMPLETED_LINE) {
+				if(myMove) {
+					boardState[row][col] = 1;
+					aiscore = aiscore + 1;
+				} else {
+					boardState[row][col] = -1;
+					playerscore = playerscore + 1;
+				}
+				return true;
+			} else {
+				return false;
+			}
 		}
-		else return false;
+		return true;
 	}
-
 }
--- src/Minimax.java
+++ src/Minimax.java
@@ -5,80 +5,56 @@ import java.util.LinkedList;
 //This class handles the AI movements
 public class Minimax {
 
-	Board b;
-
-	Minimax (Board b) {
-		this.b = b;
-		this.b.aimove = true;
-	}
+	Minimax () {}
 
 	//Move is called from main to initiate AI Move
-	public Board move () {
-		//Grabs current board
-		Node current = new Node(b);
-		//Initiates minimax search with current board and specified depth
-		Node minimax = search(current, 0);
-		//Traverses back up the tree to find the move to make
-		Node move = getMove(minimax);
-		//Returns the board after move has been made
-		return move.board;
+	public int[] getBestMove(Board b) {
+		
+		return search(b, 0, true, Integer.MIN_VALUE, Integer.MAX_VALUE);
+
 	}
 
-	//Recursive Minimax Search, returns Node at max depth
-	public Node search (Node current, int depth) {
-		if (depth > depth){
-			return current;
+	// returns an int of size 3 where int[2] is the minimax eval, 
+	// int[0] is the row of the line, and int[1] is the column of the line (in our 19x19 array)
+	public int[] search(Board b, int depth, boolean isMaxing, int alpha, int beta) {
+
+		int[] curLine = b.lastLine;
+		if(curLine == null) {
+			curLine = b.getLegalMoves().get(0);
 		}
 
-		boolean ai = false;
+		List<Board> children = getChildren(b);
 
-		//At every other depth moves alternate between Player and AI
-		if (depth % 2 == 0) {
-			ai = false;
+		if(children.isEmpty() || depth == 5) {
+			return new int[]{curLine[0], curLine[1], b.evaluate()};
 		}
 
-		//Gets successors of current node (possible moves)
-		List<Node> children = getSuccessors(current, ai);
+		if(isMaxing) {
 
-		Node tempNode = null;
-
-		if (current.board.isAIMove()) {
-			Integer value = Integer.MIN_VALUE;
-
-			for (Node child : children) {
-
-				//Runs evaluation function
-				child.board.evaluate();
-
-				//Recurses down tree until max depth is reached for a child, then does comparisons
-				Node x = search(child, depth + 1);
-				if (x.board.evaluate() > value) {
-					tempNode = x;
-					value = x.board.evaluate();
+			int[] bestMove = new int[] {curLine[0], curLine[1], Integer.MIN_VALUE};
+			for(Board child : children) {
+				int[] aMove = search(child, depth + 1, child.myMove, alpha, beta);
+				bestMove[2] = Math.max(bestMove[2], aMove[2]);
+				alpha = Math.max(bestMove[2], alpha);
+				if(alpha >= beta) {
+					break;
 				}
 			}
-			return tempNode;
-		}
+			return bestMove;
 
-		//If its Player's depth/move, find the min node based on the difference in scores
-		else {
+		} else {
 
-			Integer value = Integer.MAX_VALUE;
-			for (Node child : children) {
-                
-				//Runs evaluation function
-				child.board.evaluate();
-
-				//Recurses down tree until max depth is reached for a child, then does comparisons
-				Node x = search(child, depth + 1);
-				if (x.board.evaluate() < value) {
-					tempNode = x;
-					value = x.board.evaluate();
+			int[] bestMove = new int[] {curLine[0], curLine[1], Integer.MAX_VALUE};
+			for(Board child : children) {
+				int[] aMove = search(child, depth + 1, child.myMove, alpha, beta);
+				bestMove[2] = Math.min(bestMove[2], aMove[2]);
+				beta = Math.min(bestMove[2], beta);
+				if(alpha >= beta) {
+					break;
 				}
 			}
-			return tempNode;
+			return bestMove;
 		}
-
 	}
 
 	public Node getMove (Node current) {
@@ -90,45 +66,9 @@ public class Minimax {
 		return tempNode;
 	}
 
-	public List<Node> getSuccessors(Node state, boolean value) {
-
-		List<Node> children = new ArrayList<>();
-		Board x = state.board;
-		int rows = x.rows;
-		int cols = x.cols;
-
-		int[][] board = x.getState();
-
-		for (int i = 0; i < rows; i ++) {
-			for (int j = 0; j < cols; j++) {
-				if ((i % 2 == 0 && j % 2 != 0) && board[i][j] == 7) {
-					int[][] temp = copyArray(board, rows, cols);
-					temp[i][j] = 9;
-					Board tmp = new Board(temp, x.rows, x.cols, x.playerscore, x.aiscore, value, x.totallines, x.maxlines);
-					tmp.updatescore(i, j, "horizontal");
-					tmp.totallines++;
-					Node child = new Node(tmp);
-					child.setParent(state);
-					children.add(child);
-				}
-				else if ((i % 2 != 0 && j % 2 == 0) && board[i][j] == 7) {
-					int[][] temp = copyArray(board, rows, cols);
-					temp[i][j] = 11;
-					Board tmp = new Board(temp, x.rows, x.cols, x.playerscore, x.aiscore, value, x.totallines, x.maxlines);
-					tmp.updatescore(i, j, "vertical");
-					tmp.totallines++;
-					Node child = new Node(tmp);
-					child.setParent(state);
-					children.add(child);
-				}
-			}
-		}
-		return children;
-	}
-
-    public List<Node> getChildren(Node cur) {
-        LinkedList<Node> children = new LinkedList<Node>();
-        int[][] curState = cur.board.getState();
+    public List<Board> getChildren(Board b) {
+        LinkedList<Board> children = new LinkedList<Board>();
+        int[][] curState = b.getState();
         for(int row = 0; row < 19; row++) {
             for(int col = 0; col < 19; col = col + 2) {
                 if(row % 2 == 0) {
@@ -136,11 +76,11 @@ public class Minimax {
                 }
                 if(curState[row][col] == Board.EMPTY_LINE) {
                     int[][] newState = copyArray(curState, curState.length, curState.length);
-                    newState[row][col] = Board.COMPLETED_LINE;
-                    Board newB = new Board(newState, cur.board.playerscore, cur.board.aiscore);
-                    Node newN = new Node(newB);
-                    newN.setParent(cur);
-                    children.add(newN);
+                    Board newB = new Board(newState, b.playerscore, b.aiscore, !b.myMove);
+					if(newB.completeLine(row, col)) {
+						newB.myMove = !newB.myMove;
+					}
+                    children.add(newB);
                 }
             }
         }
